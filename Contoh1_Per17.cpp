f62097b66c0668d4b2867b588e71caa44bc6df5f
icallll26 <isalmuhamad26@gmail.com>
2025-06-13 19:17:21 +0700

    Add files via upload

---
+++ Contoh1_Per17.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+void garis() {
+    printf("\n----------------------\n");
+}
+
+int main() {
+    system("cls");
+    garis();
+    cout << "AMIK BSI - Pondok Labu" << endl;
+    garis();
+    system("pause");
+    return 0;
+}
+
